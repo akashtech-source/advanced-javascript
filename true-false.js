753c485f168c4f35fe45f3eb562662f0972cff29
akashtech-source <mr.ifranakash@gmail.com>
2021-02-02 01:54:24 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy :
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy:
+// Any number other than 0
+// "anything in string"
+// [] ARRAY
+// 
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("condtion true");
+}
+else{
+    console.log("condition false");
+}
